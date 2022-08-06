84337d50ca05a802e87d5215d701b8fb328ad991
Sbuzlouf <sumayaq8.06@gmail.com>
2022-08-06 22:58:52 +0300

    if & onTapGesture used

--- calculator/calculator/ContentView.swift
+++ calculator/calculator/ContentView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+    @State var grade = ""
+    @State var level = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.red
+                .opacity(0.6)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Calculator")
+                    .font(Font.custom("Mishafi Regular", size: 30))
+                    .padding()
+                    
+                Spacer()
+                
+                Image("calculator")
+                     .resizable()
+                     .scaledToFit()
+                     .padding()
+                              
+                TextField("ENTER YOUR GRADE", text: $grade)
+                     .padding()
+                     .background(.white)
+                               
+                Text("calculate my grade")
+                    .frame(width:200,height:60)
+                    .font(.system(size:20))
+                    .background(.black)
+                    .foregroundColor(.white)
+                    .onTapGesture {
+                        if (Int(grade) ?? 0) >= 90 {
+                            level = "امتياز"
+                        }
+                        else if (Int(grade) ?? 0) >= 80 {
+                            level = "جيد جداً"
+                        }
+                        else if (Int(grade) ?? 0) >= 70 {
+                            level = "جيد"
+                        }
+                        else if (Int(grade) ?? 0) >= 60 {
+                            level = "مقبول"
+                        }
+                        else {
+                            level = "راسب"
+                        }
+                    }
+                
+                    Text("لقد حصلت على")
+                        .font(.system(size: 30))
+                
+                    Spacer()
+            
+                    Text(level)
+                        .font(Font.custom("Mishafi Regular", size: 80))
+                        .foregroundColor(.blue)
+            }
+        }
     }
 }
 
